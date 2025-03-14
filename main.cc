867f04f75218c4b6055d99c9b4054214e1071750
Leonya <el-leonya@yandex.ru>
2025-03-14 15:29:24 +0300

    feat: add drogon to project

--- main.cc
+++ main.cc
@@ -1,6 +1,18 @@
-#include <iostream>
+#include <drogon/drogon.h>
+#include <trantor/utils/Logger.h>
 
 int main() {
-    std::cout << "Hello Yocto!" << std::endl;
+    using Callback = std::function<void (const drogon::HttpResponsePtr &)> ;
+
+    drogon::app().registerHandler("/", [](const drogon::HttpRequestPtr& req, Callback &&callback)
+    {
+        auto resp = drogon::HttpResponse::newHttpResponse();
+        resp->setBody("Hello World");
+        callback(resp);
+    });
+
+    LOG_INFO << "Server running on 127.0.0.1:8848";
+    drogon::app().addListener("127.0.0.1", 8848).run();
+
     return 0;
 }
